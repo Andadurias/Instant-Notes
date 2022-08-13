56433dbb197759fb9273b8b5bc07c8c1b267894e
Blanca <blancacano2.71828@gmail.com>
2022-08-13 13:53:55 +0200

    🛠 Inserte a card inside database #9

--- backend/fastapi-docker/app/endpoints/insert.py
+++ backend/fastapi-docker/app/endpoints/insert.py
@@ -8,12 +8,9 @@ keyspace = config['memories']['keyspace']
 _port = int(config['memories']['port'])
 ip = config['memories']['ip']
 
-cluster = Cluster([ip],port=_port)
-session = cluster.connect(keyspace, wait_for_all_pools=True)
-session.execute(f'USE {keyspace}')
-
 def insert_card(content):
-    '''Content should be a dictionary with: 
+    '''Insert a cart inside the database
+    Content should be a dictionary with: 
     card_id uuid,
     user_id uuid,
     card text, 
@@ -24,30 +21,23 @@ def insert_card(content):
     creation_time timestamp, 
     last_update timestamp
     '''
-    '''
     cluster = Cluster([ip],port=_port)
     session = cluster.connect(keyspace, wait_for_all_pools=True)
-    '''
     session.execute(f'USE {keyspace}')
+
     session.execute(
         """
         INSERT INTO store.cards 
-        (user_id, card_id, card,  creation_time, last_update )
-        VALUES (%s, uuid(), %s, toTimeStamp(now()), toTimeStamp(now()))
+        (user_id, card_id, card,  creation_time, last_update, explicit_tags, implicit_tags)
+        VALUES (%s, uuid(), %s, toTimeStamp(now()), toTimeStamp(now()), %s, %s)
         """,
         (uuid.UUID(content['user_id']), 
         content['card'],
-        #content['explicit_tags'], content['implicit_tags']
+        set(content['explicit_tags']), 
+        set(content['implicit_tags']),
         )
     )
+    cluster.shutdown()
 
-if __name__ == "__main__":
-    content = {
-        'user_id':'e04c9e80-194c-11ed-93ff-c57d679aad6d',
-        'card' : " 22222222 222 Came we need to finish memories"
-    }
 
-    insert_card(content)
-    rows = session.execute('SELECT * FROM cards')
-    for row in rows:
-        print(row)
+    
