13180c8e035fe1aa340ad8752c6af163cbe4eef0
Blanca <blancacano2.71828@gmail.com>
2022-08-25 12:46:51 +0200

    🛠 Frontend: Menu navigation. #12 The first screen consist on three button to go to: - create cards screen - remainders - search All those view are not done

--- frontend/mobile_app_react_native/App.tsx
+++ frontend/mobile_app_react_native/App.tsx
@@ -1,51 +1,22 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import styled from 'styled-components';
 
-const StyledButton = styled(Button)`
-  color: palevioletred;
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-`;
-function HomeScreen({navigation} : any) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen({ navigation } : any) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-      <StyledButton title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
-
+import {HomeScreen} from './views/home/home';
+import { CreateCardView } from './views/create_card/create_card';
+import { menu } from './constants/languajes/EN/menu';
+import { RemaindersView } from './views/remainders/remainders';
+import { SearchView } from './views/search/search';
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Navigator initialRouteName={menu.home} >
+        <Stack.Screen name={menu.home} component={HomeScreen} />
+        <Stack.Screen name={menu.card_adding} component={CreateCardView} />
+        <Stack.Screen name={menu.reminders} component={RemaindersView} />
+        <Stack.Screen name={menu.search} component={SearchView} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ frontend/mobile_app_react_native/constants/languajes/EN/menu.ts
@@ -0,0 +1,6 @@
+export const menu = {
+    home: "Home",
+    card_adding : "Add cards",
+    reminders : "Reminders",
+    search : "search"   
+}
