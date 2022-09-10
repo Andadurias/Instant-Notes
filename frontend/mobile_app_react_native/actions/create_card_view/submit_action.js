f86052b59a759a6a4e707d87109dd576fb2d08e5
Blanca <blancacano2.71828@gmail.com>
2022-09-10 12:20:25 +0200

    🔆 Test of creaction correctly made Now you can create a table #19

--- frontend/mobile_app_react_native/actions/create_card_view/submit_action.js
+++ frontend/mobile_app_react_native/actions/create_card_view/submit_action.js
@@ -1,21 +1,30 @@
 import { Alert } from "react-native";
-import CreateDatabase from "../../local_storage/create_database";
-import requirements from "./submit_requirement_test";
-import * as local_storage from "../../local_storage/main";
 
-function submitAction(tags, text, tagsSetter, textSetter){
-    return ()=>{
-        if (requirements(tags, text)){
-            CreateDatabase('test')
-            Alert.alert("Save correctly")
+import requirements from "./submit_requirement_test";
+import {createTable, getDBConnection} from "../../local_storage/main";
+import { enablePromise } from "react-native-sqlite-storage";
+
+const submitAction = async (tags, text, tagsSetter, textSetter) => {
+        if (requirements(tags, text)){ // Field not empty
             tagsSetter("")
             textSetter("")
+             
+            enablePromise(true)
+            try{
+                const db = await getDBConnection();
+                await createTable(db);
+                Alert.alert("Save correctly")
 
+            } catch(error){
+                Alert.alert("Error")
+                console.error(error);
+            }
             
         }
-        else
+        else{
             Alert.alert("Error","Fields should contain letters")
-    }
+        }
 }
+
 
 export default submitAction;
--- frontend/mobile_app_react_native/local_storage/createTable.ts
+++ frontend/mobile_app_react_native/local_storage/createTable.ts
@@ -1,12 +1,14 @@
 import {SQLiteDatabase} from 'react-native-sqlite-storage';
+import { tableName } from '../constants/local_storage';
 
 const createTable = async (db: SQLiteDatabase) => {
   // create table if not exists
-  const query = `CREATE TABLE IF NOT EXISTS Test(
+  const query = `CREATE TABLE IF NOT EXISTS ${tableName}(
         value TEXT NOT NULL
     );`;
 
   await db.executeSql(query);
 };
+// TODO we need to adapt the port names
 
 export default createTable;
