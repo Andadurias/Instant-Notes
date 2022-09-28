cd759f3be3e8289f5071e45983f5f2c01515bb25
Blanca <blancacano2.71828@gmail.com>
2022-09-29 00:17:33 +0200

    ✨ Adding multiples tags fixed #34 Finally the empty spaces have been remove Now tags accepted number since it is not a major problem

--- frontend/mobile_app_react_native/__tests__/actions/tagsProcessing.test.ts
+++ frontend/mobile_app_react_native/__tests__/actions/tagsProcessing.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import tagsProcessing, { tagsProcessingListToList } from "../../actions/tagsProcessing/tagsProcessing"
+import tagsProcessing, { tagsProcessingListToList, testToAddTagToList } from "../../actions/tagsProcessing/tagsProcessing"
 
 
 describe('Testing tags processing', () => {
@@ -13,12 +13,36 @@ describe('Testing tags processing', () => {
                 ["hola", "soy", "yo"]
             )
         )
+        it('Should not be empty', () =>
+            expect(
+                tagsProcessing("Aa;   ;11 ;")
+            ).deep.equal(
+                ["aa","11"]
+            )
+        )
     })
     describe('List of tags to list of tags', () => {
         it('Should be lower and trimmed', () =>
             expect(
                 tagsProcessingListToList(["hOla ", " Y O "])
             ).deep.equal(["hola","y o"])
+        )
+    })
+    describe('Some requirement to add a tag to a list' , () => {
+        it('Should not be empty', () =>
+            expect(
+                testToAddTagToList(' ', ["hello"])
+            ).equal(false)
+        );
+        it('Should add a word', () =>
+            expect(
+                testToAddTagToList('tomate', ["hello"])
+            ).equal(true)
+        )
+        it('Should not repeat ', () =>
+            expect(
+                testToAddTagToList('hello', ["hello"])
+            ).equal(false)
         )
     })
 })
--- frontend/mobile_app_react_native/components/TagInput.tsx/AddButton.tsx
+++ frontend/mobile_app_react_native/components/TagInput.tsx/AddButton.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "native-base";
 import { interfaceTagStructure } from "../../views/search/search";
+import { testToAddTagToList } from "../../actions/tagsProcessing/tagsProcessing";
 /**
  * AddButton 
  * @param tag The tag that have been written on the input
@@ -11,14 +12,18 @@ import { interfaceTagStructure } from "../../views/search/search";
  */
 const AddButton = (tag: String, setTag, list: interfaceTagStructure, listSetter) => {
     const addTag = ( tag: String, setTag, list: interfaceTagStructure, listSetter) => {
-        let new_list = list.list
-        new_list.push(tag)
-        listSetter(
-            {
-                state: (list.state == false),
-                list : new_list
-            }
-        )
+        
+        // tag preprocessing needed
+        if( testToAddTagToList(tag, list.list)){
+            let new_list = list.list
+            new_list.push(tag)
+            listSetter(
+                {
+                    state: (list.state == false),
+                    list : new_list
+                }
+            )
+        }
         setTag("")
     }
     return (
