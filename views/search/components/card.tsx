48c1317f272e296262e5eb7171e816579867daf6
Blanca <blancacano2.71828@gmail.com>
2022-11-14 13:43:14 +0100

    Now if a card is pushed it span #50

--- views/search/components/card.tsx
+++ views/search/components/card.tsx
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, VStack,Text, HStack } from "native-base";
-import { StyleSheet } from "react-native";
+import { Pressable, SafeAreaView, StyleSheet, View } from "react-native";
 
 function Card({content, date, id}){
-    return (
-      <Box style={styles.card}>
-        <VStack>
-          <Box>
-            <Text>{content}</Text>
-          </Box>
-          <HStack>
-            <Text>Date: </Text>
-            <Text>{date}</Text>
-            <Text>  Id: </Text>
-            <Text>{id}</Text>
-          </HStack>
-        </VStack>
-      </Box>           
-    )
+  const fullContent = content
+  const [showAll, setShowAll] = useState(false)
+  const [currentContent, setCurrentContent] = useState(content)
+  const substringLength = 40
+  useEffect(() => {
+    if(showAll)
+      setCurrentContent(fullContent)
+    else 
+    setCurrentContent(fullContent.substring(0,substringLength))
+  }, [showAll]);
+
+  return (
+    <SafeAreaView style={styles.card}>
+        <Pressable onPress={
+            () => setShowAll(! showAll)
+          }>
+          <Text>
+            {currentContent}
+          </Text>
+          <Text>Date: {date}       Id: {id}</Text>
+        </Pressable>
+    </SafeAreaView>
+  )
 }
 export default Card;
 
